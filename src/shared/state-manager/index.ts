9fa3358e4e4dee26d9ddeb7bc9ceb466848fe00c
Brad Anderson <>
2019-06-09 18:22:00 -0400

    Fixes a bunch of type issues.

--- src/shared/state-manager/index.ts
+++ src/shared/state-manager/index.ts
@@ -21,6 +21,7 @@ export interface Dispatch<T> {
 
 export interface Store<T, U = T> {
   getState: () => T;
+  getTransformedState: () => U;
   dispatch: Dispatch<T>;
   subscribe: (listener: (newState: U) => void) => Unsubscribe;
 }
@@ -38,6 +39,7 @@ export const createStore = <T>(initialState: T, ...middleware: Middleware<T>[]):
   const listeners: Listener<T>[] = [];
 
   const getState = () => state;
+  const getTransformedState = getState;
   const dispatch = (setters: Setter<T> | { [K in keyof T]?: Setter<T[K]> }) => {
     const newState = (typeof setters === 'function')
       ? setters(state)
@@ -58,19 +60,25 @@ export const createStore = <T>(initialState: T, ...middleware: Middleware<T>[]):
     };
   };
 
-  return { getState, dispatch, subscribe };
+  return { getState, getTransformedState, dispatch, subscribe };
 };
 
-export const mapStore = <T, U>(store: Store<T>, map: (state: T) => U): Store<T, U> => {
-  const subscribe = (listener: Listener<U>) => {
+export const mapStore = <T, U, V>(store: Store<T, U>, map: (state: U) => V): Store<T, V> => {
+  const getTransformedState = () => map(store.getTransformedState());
+  const subscribe = (listener: Listener<V>) => {
     return store.subscribe(state => listener(map(state)));
   };
-  return { ...store, subscribe };
+  return { ...store, getTransformedState, subscribe };
 };
 
-export const composeStore = <T, U extends { [K in keyof T]: any }>(stores: { [K in keyof T]: Store<T[K], U[K]> }): Store<T, U> => {
-  const keys = Object.keys(stores) as Array<keyof typeof stores>;
-  const getState = () => keys.reduce((acc, key) => Object.assign(acc, { [key]: stores[key].getState() }), {}) as U;
+type StoreDict<T, U> =
+  & { [K in keyof T]: Store<T[K], any> }
+  & { [K in keyof U]: Store<any, U[K]> };
+
+export const composeStore = <T, U>(stores: StoreDict<T, U>): Store<T, U> => {
+  const keys = Object.keys(stores) as Array<keyof T>;
+  const getState = () => keys.reduce((acc, key) => Object.assign(acc, { [key]: stores[key].getState() }), {}) as T;
+  const getTransformedState = () => keys.reduce((acc, key) => Object.assign(acc, { [key]: stores[key].getTransformedState() }), {}) as U;
   const dispatch = (setters: Setter<T> | { [K in keyof T]?: Setter<T[K]> }) => {
     const newState = (typeof setters === 'function')
       ? setters(getState())
@@ -81,9 +89,9 @@ export const composeStore = <T, U extends { [K in keyof T]: any }>(stores: { [K 
   };
   const subscribe = (listener: Listener<U>) => {
     const subscriptions = keys.map(key => stores[key].subscribe(newState => {
-      listener({ ...getState(), [key]: newState });
+      listener({ ...getTransformedState(), [key]: newState });
     }));
     return () => subscriptions.forEach(unsubscribe => unsubscribe());
   };
-  return { getState, subscribe, dispatch };
+  return { getState, getTransformedState, subscribe, dispatch };
 };
