84f06595daee438b6f19edddd897808c823ed464
Brad Anderson <>
2019-06-08 21:29:22 -0400

    Adds state-manager

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,28 @@
+import { Setter, createStore, mapStore, composeStore } from 'shared/state-manager';
 import 'index.css';
+
+const add = (n: number): Setter<number> => s => s + n;
+const addName = (name: string): Setter<string[]> => s => [ ...s, name ];
+const priceStore = createStore(1);
+const mappedStore = mapStore(priceStore, n => '$' + n.toFixed(2));
+const nameStore = createStore([ 'phil' ]);
+const compoundStore = composeStore({ mapped: mappedStore, name: nameStore });
+
+mappedStore.subscribe(s => console.log('mappedStore', s));
+priceStore.subscribe(s => console.log('priceStore', s));
+compoundStore.subscribe(s => console.log('compoundStore', s));
+
+const test = () => {
+  console.log('=== dispatch mappedStore');
+  mappedStore.dispatch(add(1));
+
+  console.log('=== dispatch priceStore');
+  priceStore.dispatch(add(1));
+
+  console.log('=== dispatch compoundStore');
+  compoundStore.dispatch(s => ({ ...s, name: addName('lkj')(s.name) }));
+
+  console.log('=== dispatch byKey');
+  compoundStore.dispatch({ name: addName('abc') });
+};
+test();
---
+++ src/shared/state-manager/index.ts
@@ -0,0 +1,87 @@
+export interface Setter<T> {
+  (state: T): T;
+}
+
+export interface Middleware<T> {
+  (state: T, prevState?: T): T;
+}
+
+export interface Listener<T> {
+  (newState: T): void;
+}
+
+export interface Unsubscribe {
+  (): void;
+}
+
+export interface Store<T, U = T> {
+  getState: () => T;
+  dispatch: {
+    (setter: Setter<T>): void;
+    (setters: { [K in keyof T]?: Setter<T[K]> }): void;
+  }
+  subscribe: (listener: (newState: U) => void) => Unsubscribe;
+}
+
+const byKey = <T>(state: T, setters: { [K in keyof T]?: Setter<T[K]> }): T => {
+  const keys = Object.keys(setters) as Array<keyof typeof setters>;
+  return keys.reduce(
+    (acc, key) => Object.assign(acc, { [key]: setters[key]!(state[key]) }),
+    { ...state },
+  );
+};
+
+export const createStore = <T>(initialState: T, ...middleware: Middleware<T>[]): Store<T> => {
+  let state: T = middleware.reduce((s, m) => m(s), initialState);
+  const listeners: Listener<T>[] = [];
+
+  const getState = () => state;
+  const dispatch = (setters: Setter<T> | { [K in keyof T]?: Setter<T[K]> }) => {
+    const newState = (typeof setters === 'function')
+      ? setters(state)
+      : byKey(state, setters);
+    const prevState = state;
+    if (newState !== prevState) {
+      state = middleware.reduce((s, m) => m(s, prevState), newState);
+      listeners.forEach(listener => listener(state));
+    }
+  };
+  const subscribe = (listener: Listener<T>): Unsubscribe => {
+    listeners.push(listener);
+    return () => {
+      const index = listeners.indexOf(listener);
+      if (index > -1) {
+        listeners.splice(index, 1);
+      }
+    };
+  };
+
+  return { getState, dispatch, subscribe };
+};
+
+export const mapStore = <T, U>(store: Store<T>, map: (state: T) => U): Store<T, U> => {
+  const subscribe = (listener: Listener<U>) => {
+    return store.subscribe(state => listener(map(state)));
+  };
+  return { ...store, subscribe };
+};
+
+export const composeStore = <T, U extends { [K in keyof T]: any }>(stores: { [K in keyof T]: Store<T[K], U[K]> }): Store<T, U> => {
+  const keys = Object.keys(stores) as Array<keyof typeof stores>;
+  const getState = () => keys.reduce((acc, key) => Object.assign(acc, { [key]: stores[key].getState() }), {}) as U;
+  const dispatch = (setters: Setter<T> | { [K in keyof T]?: Setter<T[K]> }) => {
+    const newState = (typeof setters === 'function')
+      ? setters(getState())
+      : byKey(getState(), setters);
+    keys.forEach(key => {
+      stores[key].dispatch(() => newState[key]);
+    });
+  };
+  const subscribe = (listener: Listener<U>) => {
+    const subscriptions = keys.map(key => stores[key].subscribe(newState => {
+      listener({ ...getState(), [key]: newState });
+    }));
+    return () => subscriptions.forEach(unsubscribe => unsubscribe());
+  };
+  return { getState, subscribe, dispatch };
+};
